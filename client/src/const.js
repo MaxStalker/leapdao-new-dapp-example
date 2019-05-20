39c4a779cb0aeaad7f945d2951093a94ba1d1644
max <max.daunarovich@gmail.com>
2019-05-21 02:01:33 +0300

    Add plasma related methods and constants

---
+++ client/src/const.js
@@ -0,0 +1,5 @@
+// Token Address: LEAP
+export const TOKEN_ADDRESS = "0x0666eBbF26CDE07EA79FeCAe15e5f18394EBC149";
+
+// RPC Server: Staging
+export const PLASMA_PROVIDER = "https://staging-testnet.leapdao.org/rpc";
---
+++ client/src/plasma.js
@@ -0,0 +1,31 @@
+import Web3 from "web3";
+import * as ethers from "ethers";
+import { helpers } from "leap-core";
+import { PLASMA_PROVIDER, TOKEN_ADDRESS } from "./const";
+import TOKEN_ABI from "./contracts/erc20Abi";
+
+let tokenColor = null;
+const rpc = new ethers.providers.JsonRpcProvider(PLASMA_PROVIDER);
+const plasma = helpers.extendWeb3(new Web3(PLASMA_PROVIDER));
+const tokenContract = new plasma.eth.Contract(TOKEN_ABI, TOKEN_ADDRESS);
+
+// If installed, Metamask will expose ethereum object on window
+const { ethereum } = window;
+//const wallet = ethereum ? new Web3(ethereum) : null;
+
+export const getBalance = async account => {
+  const balance = await tokenContract.methods.balanceOf(account).call();
+  console.log("Balance:", balance);
+  return balance;
+};
+
+export const getColor = async () => {
+  if (!tokenColor) {
+    tokenColor = await plasma.getColor(TOKEN_ADDRESS);
+  }
+  return tokenColor;
+};
+
+export const activateWallet = async () => {
+  return await ethereum.enable();
+};
