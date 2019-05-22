98ab77aa739e7cf02f7791ea07491191875dc541
max <max.daunarovich@gmail.com>
2019-05-22 16:55:14 +0300

    Add example files for necessary mnemonics

---
+++ server/integration/mnemonics.example.js
@@ -0,0 +1,11 @@
+// Add mnemonic here before running the game
+// You can generate mnemonics by running
+// "node wallet/scripts/generateWallet.js"
+// then pasting words here and topping wallets with faucet later
+const HOUSE_WALLET_MNEMONIC = "";
+const PLAYER_WALLET_MNEMONIC = "";
+
+module.exports = {
+  HOUSE_WALLET_MNEMONIC,
+  PLAYER_WALLET_MNEMONIC
+};
