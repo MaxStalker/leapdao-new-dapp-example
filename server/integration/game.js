ee36344eb82102f323c5c1748ab697bdb380c7cd
max <max.daunarovich@gmail.com>
2019-05-23 01:34:37 +0300

    Update game integration test

--- server/integration/game.js
+++ server/integration/game.js
@@ -1,21 +1,27 @@
 const got = require("got");
 const ethers = require("ethers");
-const { ripemd160 } = require("ethereumjs-util");
+const { Tx, Input, Output } = require("leap-core");
 const {
   HOUSE_WALLET_MNEMONIC,
   PLAYER_WALLET_MNEMONIC
 } = require("./mnemonics");
-const { showLog, replaceAll, toEth } = require("../utils/utility");
-const { fundGas } = require("../wallet/wallet");
+const { showLog, toEth } = require("../utils/generic");
 const {
+  rpcMessages,
   getBalance,
   getTokenColor,
+  getTokenContract,
   tokenBalanceChange,
   makeTransfer,
   getUnspentOutputs
 } = require("../wallet/rpc-proxy");
 const { RPC_URL, TOKEN_ADDRESS } = require("../wallet/config");
+const { generateNewRound } = require("../game");
+const wordGame = require("../build/contracts/WordGame");
 
+const { CHECK_CONDITION, RAW } = rpcMessages;
+
+// Faucet
 async function requestFaucet(address, color) {
   /*
   * curl -X POST -H 'Content-Type: application/json'
@@ -43,7 +49,16 @@ async function requestFaucet(address, color) {
   }
 }
 
-async function checkBalances({ playerAddress, houseAddress }) {
+// Game Loop
+async function checkBalances(balanceOf, { playerAddress, houseAddress }) {
+  const initialHouseBalance = await balanceOf(houseAddress);
+  const initialPlayerBalance = await balanceOf(playerAddress);
+  showLog({ playerAddress, houseAddress }, "Addresses");
+  showLog(
+    { house: toEth(initialHouseBalance), player: toEth(initialPlayerBalance) },
+    "Initial Balances"
+  );
+
   if (parseInt(initialPlayerBalance) === 0) {
     console.log("Player Balance is zero, requesting faucet...");
     const faucetResponse = await requestFaucet(playerAddress, 0);
@@ -56,46 +71,131 @@ async function checkBalances({ playerAddress, houseAddress }) {
     showLog({ faucetResponse }, "Player Faucet");
   }
 }
-async function createRound() {
-  return {
-    words: [],
-    roundHash: "0x1111111111111111111111111111111111111337"
-  };
-}
-async function fundCondition() {
-  return "";
-}
-async function sendAnswer() {
-  return "";
-}
 
+// Main Loop
 async function main() {
   // Setup RPC and plasma
   const plasma = new ethers.providers.JsonRpcProvider(RPC_URL);
+  const tokenContract = await getTokenContract(TOKEN_ADDRESS, plasma);
   const tokenColor = await getTokenColor(TOKEN_ADDRESS, plasma);
-  console.log("Token Color", tokenColor);
   const balanceOf = getBalance(TOKEN_ADDRESS, plasma);
 
   // Setup House Wallet
   const houseWallet = new ethers.Wallet.fromMnemonic(HOUSE_WALLET_MNEMONIC);
   const houseAddress = houseWallet.address;
-  const initialHouseBalance = await balanceOf(houseAddress);
+  const housePrivateKey = houseWallet.privateKey;
 
   // Setup Player Wallet
   const playerWallet = new ethers.Wallet.fromMnemonic(PLAYER_WALLET_MNEMONIC);
   const playerAddress = playerWallet.address;
-  const initialPlayerBalance = await balanceOf(playerAddress);
+  const playerPrivateKey = playerWallet.privateKey;
 
-  showLog({ playerAddress, houseAddress }, "Addresses");
-  showLog(
-    { house: toEth(initialHouseBalance), player: toEth(initialPlayerBalance) },
-    "Initial Balances"
+  await checkBalances(balanceOf, { playerAddress, houseAddress });
+  const roundBet = 100000000;
+  const round = await generateNewRound(
+    { houseWallet, playerAddress, roundBet, tokenAddress: TOKEN_ADDRESS },
+    plasma
+  );
+  const { roundScript, ...rest } = round;
+  const { answer, roundId, roundAddress, roundBalance } = rest;
+  showLog(rest, "Round generated");
+
+  // Prepare Player Answer
+  const wordGameABI = new ethers.utils.Interface(wordGame.abi);
+  const answerBytes32 = ethers.utils.formatBytes32String(answer);
+  const msgData = wordGameABI.functions.roundResult.encode([
+    answerBytes32,
+    roundId
+  ]);
+
+  // Now Player deposits his part of the bet
+  const playerTransfer = await makeTransfer(
+    {
+      from: playerAddress,
+      to: roundAddress,
+      color: tokenColor,
+      amount: roundBet,
+      privateKey: playerPrivateKey
+    },
+    plasma
   );
 
-  await checkBalances({ playerAddress, houseAddress });
-  const { words, roundHash } = await createRound();
-  const fundTxHash = await fundCondition(roundHash, plasma);
-  const resultTxHash = await sendAnswer(plasma);
+  const roundBalanceAfterPlayer = await tokenBalanceChange(
+    {
+      contract: tokenContract,
+      address: roundAddress,
+      prevBalance: roundBalance
+    },
+    plasma
+  );
+
+  console.log("\n Player funded his part. Now processing house bet \n");
+
+  const houseTransfer = await makeTransfer(
+    {
+      from: houseAddress,
+      to: roundAddress,
+      color: tokenColor,
+      amount: roundBet,
+      privateKey: housePrivateKey
+    },
+    plasma
+  );
+
+  const roundBalanceAfterHouse = await tokenBalanceChange(
+    {
+      contract: tokenContract,
+      address: roundAddress,
+      prevBalance: roundBalanceAfterPlayer
+    },
+    plasma
+  );
+
+  // Now we run spending condition
+  const utxos = await getUnspentOutputs(roundAddress, tokenColor, plasma);
+  const inputs = [
+    new Input({
+      prevout: utxos[0].outpoint,
+      script: roundScript
+    }),
+    new Input({
+      prevout: utxos[1].outpoint
+    }),
+    new Input({
+      prevout: utxos[2].outpoint
+    })
+  ];
+  const outputs = [];
+  const unlockTransaction = Tx.spendCond(inputs, outputs);
+  unlockTransaction.inputs[0].setMsgData(msgData);
+  unlockTransaction.signAll(playerPrivateKey); // TODO: check if we need sign here at all
+
+  const checkUnlockTransaction = await plasma.send(CHECK_CONDITION, [
+    unlockTransaction.hex()
+  ]);
+  showLog(checkUnlockTransaction, "Condition Check Result");
+  for (let i = 0; i < checkUnlockTransaction.outputs.length; i++) {
+    unlockTransaction.outputs[i] = new Output.fromJSON(
+      checkUnlockTransaction.outputs[i]
+    );
+  }
+
+  const checkUnlockingScriptNew = await plasma.send(CHECK_CONDITION, [
+    unlockTransaction.hex()
+  ]);
+  showLog(checkUnlockingScriptNew, "New Check");
+  const finalHash = await plasma.send(RAW, [unlockTransaction.hex()]);
+  console.log(finalHash);
+  const newRoundBalance = await tokenBalanceChange({
+    contract: tokenContract,
+    address: roundAddress,
+    prevBalance: roundBalanceAfterHouse
+  });
+
+
+  //const fundTxHash = await fundCondition(roundHash, plasma);
+  //const resultTxHash = await sendAnswer(plasma);
+  process.exit(0);
 }
 
 // TODO: Catch errors in main
