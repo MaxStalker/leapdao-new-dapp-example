b8806600060c75c1d8890ffa2f74911e3946844e
max <max.daunarovich@gmail.com>
2019-05-22 16:52:20 +0300

    Update game route with new response

--- server/routes/game.js
+++ server/routes/game.js
@@ -1,15 +1,27 @@
-var express = require('express');
-var router = express.Router();
-const getWords = require('random-words');
+const express = require("express");
+const router = express.Router();
+const ethers = require('ethers');
+const getWords = require("random-words");
+const { fundGas, fundCondition } = require("../wallet/wallet");
+
+const newRoundAddress = (words, lockWord) =>{
+  const lockBytes32 = ethers.utils.formatBytes32String(lockWord);
+  console.log(lockBytes32);
+
+  return ""
+};
 
 /* GET home page. */
-router.get('/startRound', (req, res) =>{
-  const words = getWords({exactly: 4, maxLength: 8});
-
+router.get("/startRound", (req, res) => {
+  const words = getWords({ exactly: 4, maxLength: 8 });
+  const pick = Math.floor(Math.random() * 4);
+  const lockWord = words[pick];
+  const roundAddress = newRoundAddress(words, lockWord);
+  console.log({roundAddress});
   // 1) fund transaction
   // 2) send transaction address
 
-  res.status(200).send({words});
+  res.status(200).send({ roundAddress, words });
 });
 
 module.exports = router;
