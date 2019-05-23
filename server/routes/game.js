da0180222c50806347af5102a28505f42e48df31
max <max.daunarovich@gmail.com>
2019-05-24 02:24:57 +0300

    Add new route finishRound

--- server/routes/game.js
+++ server/routes/game.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const ethers = require("ethers");
-const { generateNewRound } = require("../game");
+const { generateNewRound, finishRound } = require("../game");
 const { getPlasma, getWallet } = require("../wallet/wallet");
 const { TOKEN_ADDRESS } = require("../wallet/config");
+
+const rounds = {};
 
 /* GET home page. */
 router.post("/startRound", async (req, res) => {
@@ -16,13 +17,29 @@ router.post("/startRound", async (req, res) => {
     { houseWallet, playerAddress, tokenAddress, roundBet },
     plasma
   );
-  console.log(round);
-  const { roundAddress, roundId, words, codeBuffer } = round;
-  console.log({ codeBuffer });
-  // 1) fund transaction
-  // 2) send transaction address
+  const { roundAddress } = round;
+  rounds[roundAddress] = round;
 
-  res.status(200).send({ roundAddress, words, roundId, codeBuffer });
+  res.status(200).send(round);
+});
+
+router.post("/finishRound", async (req, res) => {
+  const { roundAddress } = req.body;
+  const round = rounds[roundAddress];
+  if (!round) {
+    res.status(500).send(JSON.stringify({ message: "Round does not exist" }));
+  }
+  const roundBet = 100000000;
+  const plasma = getPlasma();
+  const houseWallet = getWallet();
+  const tokenAddress = TOKEN_ADDRESS;
+  const { word } = req.body;
+  const roundStatus = await finishRound(
+    { word, round, roundBet, houseWallet, tokenAddress },
+    plasma
+  );
+  const {answer} = round;
+  res.status(200).send({...roundStatus, answer});
 });
 
 module.exports = router;
